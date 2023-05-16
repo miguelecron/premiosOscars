80b2d637810bd95fa248731282314fed998958f7
miguelecron <miguelsoler@gmx.es>
2023-05-16 10:10:32 +0200

    Arreglado el comparador de películas

--- src/main/java/CompararPeliculasAnios.java
+++ src/main/java/CompararPeliculasAnios.java
@@ -3,6 +3,6 @@ import java.util.Comparator;
 public class CompararPeliculasAnios implements Comparator<Pelicula> {
     @Override
     public int compare(Pelicula p1, Pelicula p2) {
-        return p1.getAnio() - p2.getAnio();
+        return p2.getAnio() - p1.getAnio();
     }
 }
